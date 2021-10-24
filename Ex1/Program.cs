8dc5e4ae615244c8b905993d84f600999e670abd
USER <USER@GEE0S>
2021-10-24 15:46:05 +0200

    Добавьте файлы проекта.

---
+++ Ex1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string str1 = "([]{})[]";
+            string str2 = "([]]";
+            string str3 = "([{ }])";
+            string str4 = "([{";
+            Console.WriteLine(CheckBrackets(str1));
+            Console.WriteLine(CheckBrackets(str2));
+            Console.WriteLine(CheckBrackets(str3));
+            Console.WriteLine(CheckBrackets(str4));
+            Console.ReadKey();
+        }
+        public static bool CheckBrackets(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in str)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                    continue;
+                }
+                if (c == ')' && stack.Peek() == '(')
+                    stack.Pop();
+
+                if (c == ']' && stack.Peek() == '[')
+                    stack.Pop();
+                if (c == '}' && stack.Peek() == '{')
+                    stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
